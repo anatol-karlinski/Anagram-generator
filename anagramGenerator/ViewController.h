295428b90762e9d6bde98f40c9776b722d6e090c
anatol-karlinski <anatol.karlinski@icloud.com>
2015-12-18 19:33:07 +0100

    First commit
    First commit

--- anagramGenerator/ViewController.h
+++ anagramGenerator/ViewController.h
@@ -9,6 +9,15 @@
 #import <Cocoa/Cocoa.h>
 
 @interface ViewController : NSViewController
+@property (weak) IBOutlet NSTextField *textInput;
+
+@property (weak) IBOutlet NSTextField *textOutput;
+
+- (IBAction)generateButton:(NSButton *)sender;
+- (IBAction)zapiszButton:(id)sender;
+- (IBAction)find:(NSButton *)sender;
+@property (weak) IBOutlet NSTextField *lenght;
+
 
 
 @end
